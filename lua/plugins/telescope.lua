71977daf07f73bf51d91c58853df75d0e6f600b9
Abhishek <abhisheksatyam123@gmail.com>
2026-07-04 15:54:28 +0530

    Disable Telescope keymaps to prioritize fzf-lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -34,9 +34,7 @@ return {
       },
     })
 
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
-    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
-    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+    -- Telescope keymaps disabled to prioritize fzf-lua.
+    -- Telescope is kept purely as an API library for plugins (like codemarks and harpoon).
   end,
 }
