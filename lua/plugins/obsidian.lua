2e7861b52430f9ce5d86f4a0340bb578a20ad9d3
Abhishek <abhisheksatyam123@gmail.com>
2026-07-04 14:49:54 +0530

    fix(highlighting): only color yellow links that are resolved (note and code mark links)

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -75,7 +75,7 @@ return {
           ObsidianP2 = { bold = true, fg = "#ffcb6b" },
           ObsidianP3 = { bold = true, fg = "#89ddff" },
           ObsidianDeferred = { italic = true, fg = "#676e95" },
-          ObsidianRefText = { underline = true, fg = "#ffcb6b" },
+          ObsidianRefText = { underline = true },
           ObsidianExtLinkIcon = { fg = "#ffcb6b" },
           ObsidianTag = { italic = true, fg = "#89ddff" },
           ObsidianBlockID = { italic = true, fg = "#89ddff" },
